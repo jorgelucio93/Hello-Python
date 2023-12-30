c241e141e06886ca9a26a42ae1f22cdccb280535
Jorgelucio93 <jorgelucio93@gmail.com>
2023-12-30 11:02:32 -0600

    301223

--- Basic/11_classes.py
+++ Basic/11_classes.py
@@ -15,27 +15,28 @@ print(MyEmptyPerson())
 
 
 class Person:
-    def __init__(self, name, surname, alias="Sin alias"):#init para constructor de clase y esta es la forma de asignar valores.
-        self.full_name = f"{name} {surname} ({alias})"  # Propiedad pública
-        self.__name = name  # Propiedad privada
+    def __init__(self, name, surname, alias="Sin alias"):#init para constructor de clase y esta es la forma de asignar variables de la clase.
+        self.full_name = f"{name} {surname} ({alias})"  # Propiedad pública y modificable, self para asignar propiedades dentro de la clase
+        self.__name = name  # Propiedad privada y no se pueda modificar pero podemos llamarlo por lectura
 
-    def get_name(self):
+    def get_name(self):#forma de llamar por lectura una propiedad privada
         return self.__name
-
+        
     def walk(self):
         print(f"{self.full_name} está caminando")
 
 
-my_person = Person("Brais", "Moure")
+my_person = Person("Brais", "Moure")#al no definir alias toma la propiedad por defecto asignada
 print(my_person.full_name)
 print(my_person.get_name())
 my_person.walk()
 
-my_other_person = Person("Brais", "Moure", "MoureDev")
+
+my_other_person = Person("Brais", "Moure", "MoureDev")#asignacion de valores a la clase
 print(my_other_person.full_name)
 my_other_person.walk()
-my_other_person.full_name = "Héctor de León (El loco de los perros)"
+my_other_person.full_name = "Héctor de León (El loco de los perros)"#se le puede cambiar la propiedad aunque se haya definido antes otro valor
 print(my_other_person.full_name)
 
-my_other_person.full_name = 666
-print(my_other_person.full_name)
+my_other_person.full_name = [6,'10',"hola"]
+print (type(my_other_person.full_name))
---
+++ Basic/intento.py
@@ -0,0 +1,8 @@
+# Toma el número como entrada
+number = int(input())
+
+# Usa un bucle while para la cuenta regresiva
+while number >= 0:
+    print (number)
+    number = number -1
+    
