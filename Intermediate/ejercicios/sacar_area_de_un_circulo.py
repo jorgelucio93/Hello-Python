5620a6ae2441ea88dd2645e4776cead0916f6de9
Your Name <you@example.com>
2024-01-03 01:02:54 -0600

    ejercicios

---
+++ Intermediate/ejercicios/sacar_area_de_un_circulo.py
@@ -0,0 +1,5 @@
+import math
+
+radio = 3
+area = math.pi * radio ** 2
+print ("el area es:", area)
---
+++ Intermediate/ejercicios/suma_de_dos_numeros.py
@@ -0,0 +1,8 @@
+
+
+num1 = 3
+num2 = 4
+suma1 = num1 + num2
+
+
+print ("El resultado es:", suma1);
